d01887de379b8e0b31576a89da1d7b0f8015bb0f
emilioxrenders <emilioxrenders@milibook.home>
2025-01-17 16:59:04 +0100

    updated container padding

--- components/Container.vue
+++ components/Container.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div
-    :class="`container lg:pb-32 pb-16 mx-auto flex px-10 flex-col gap-8 ${
+    :class="`container lg:pb-32 pb-16 mx-auto flex px-8 flex-col gap-8 ${
       content ? 'pt-8 max-w-4xl' : 'pt-16 lg:pt-32'
     }`"
   >
