15da149f91ff4694e30f8149bea2ed0b90da2ec9
emilioxrenders <emilioxrenders@MiliBook.local>
2025-01-18 14:53:37 +0100

    updated text files

--- pages/articles/a-love-letter-to-bouldering.vue
+++ pages/articles/a-love-letter-to-bouldering.vue
@@ -45,10 +45,10 @@ useSeoMeta({
         the correct definition of bouldering, but what's the fun in that?
         Instead you will get my personal short definition. Bouldering is the art
         of climbing (mostly) colored plastic rocks on walls that are usually not
-        bigger than 5 meters. I say mostly, since outdoor climbing is also a
+        taller than 5 meters. I say mostly, since outdoor climbing is also a
         thing, but I haven't yet had the opportunity to touch real rock, so I'll
         stick to indoor climbing in this article. Bouldering involves completing
-        routes, also known as 'problems', which have a defined start and end
+        routes, also known as ‘problems', which have a defined start and end
         point. The difficulty of these routes are defined by grades.
       </p>
       <br />
@@ -61,7 +61,7 @@ useSeoMeta({
         bouldering has stopped since, and me and my other friend are still going
         strong to this day. This experience was years ago though, since at the
         time I was still preoccupied with other sports like Kendo, but I still
-        loved my very first session. Fast forward like a year and my time with
+        loved every minute of it. Fast forward like a year and my time with
         Kendo was coming to a close, and I was searching for something else to
         fill the spot. I went bouldering again in the spring of 2024 in a new
         gym that had recently opened in Tilburg, and I've been obsessed ever
@@ -72,59 +72,57 @@ useSeoMeta({
       <h2 class="mb-2">Why I love bouldering</h2>
       <p>
         What makes bouldering such a fun sport? I think there are hundreds of
-        reasons one could say, but today I will keep it limited to my personal 5
-        biggest reasons I love this sport so much.
+        reasons one could come up with, but today I will keep it limited to my
+        personal 5 biggest reasons I love this sport so much.
       </p>
       <br />
       <h3 class="mb-2">1. It's like solving a puzzle with your body</h3>
       <p>
-        If you've ever seen a bouldering competition or been to a bouldering gym
-        yourself, you've likely seen the absurd ways people use their bodies in
-        climbing. Some do a full-on split high in the air, some decompress
-        themselves to mere atoms, and others take shapes that would impress even
-        the most skilled yogis. Don't worry, this isn't something you'll likely
-        be doing in the beginning of your climbing journey, but it's a great way
-        to illustrate the puzzle solving that comes with bouldering. Every route
-        is different, and you can be as creative as you'd like in completing it.
+        If you've ever seen a bouldering competition or been to a bouldering
+        gym, you've likely seen the absurd ways people use their bodies in
+        climbing. Some do a split high in the air, some decompress themselves to
+        mere atoms, and others take shapes that would impress even the most
+        skilled yogis. Don't worry though, this isn't something you'll likely be
+        doing in the beginning of your climbing journey, but it's a great way to
+        illustrate the puzzle solving that comes with bouldering. Every route is
+        different, and you can be as creative as you'd like in completing it.
         This comparison gets made a lot, but I tend to agree with it: bouldering
         is the most video game-like sport there is.
       </p>
       <br />
       <h3 class="mb-2">2. Exercising while playing</h3>
       <p>
-        All this puzzling and problem solving isn't just great for your brain,
-        but you tend to use your entire body while doing it. It mostly focuses
-        the upper body, particularly the back and forearm muscles, but you still
-        use your legs and the rest of your body a lot. When I go bouldering, the
-        time flies by just as quickly as if I'm doing any other hobby, but that
-        still gives me roughly a 2 hour workout whilst feeling that I'm just
-        playing a game.
+        All this puzzling and problem solving is a great way to train your
+        brain, but it's also a great workout. It mostly focuses the upper body,
+        particularly the back and forearm muscles, but you still use your legs
+        and the rest of your body a lot. When I go bouldering, time flies by
+        just as quickly as if I'm doing any other hobby, but it still gives me
+        roughly a 2 hour workout whilst feeling that I'm just playing a game.
       </p>
       <br />
       <h3 class="mb-2">3. Sense of progression</h3>
       <p>
-        Especially in the beginning, the sense of progression and accomplishment
-        is one of the most rewarding things I've personally felt while
-        practicing a sport. Bouldering is so rich in its techniques and its
-        variety of routes, that especially in the beginning, you feel like
-        you're improving with each session. It's inevitable that this rate of
-        progression will slow down eventually, but there is always something to
-        work toward, a new route you want to complete, a new technique you want
-        to master. There is always a reason to keep moving forward and become
-        better than you were yesterday.
+        The sense of progression and accomplishment is one of the most rewarding
+        things I've personally experienced while practicing a sport. Bouldering
+        is so rich in its techniques and its variety of routes, that especially
+        in the beginning, you feel like you're improving with each session. It's
+        inevitable that this rate of progression will slow down eventually, but
+        there is always something to work toward, a new route you want to
+        complete, a new technique you want to master. There is always a reason
+        to keep moving forward and become better than you were yesterday.
       </p>
       <br />
       <h3 class="mb-2">4. Diversity of routes</h3>
       <p>
         I've touched on this subject shortly in some of the previous sections,
         but this deserves a highlight of its own. There is such an amazing range
-        of varied routes that you can do, that there is something for everyone.
-        There are, in my brain at least, 2 main differences in routes. The first
-        are overhanging routes, where the wall is tilted outward. Overhangs are
-        mostly focused on upper body and grip strength, since gravity pulls you
-        away from the holds. These are my personal favorite kinds of routes, and
-        they could be yours too if you want to feel like you're a spider
-        crawling out of your nest.
+        of varied routes that you can climb, that there is something for
+        everyone. There are, at least in my brain, 2 main differences in routes.
+        The first are overhanging routes, where the wall is tilted outward.
+        Overhangs are mostly focused on upper body and grip strength, since
+        gravity pulls you away from the holds. These are my personal favorite
+        kinds of routes, and they could be yours too if you want to feel like
+        you're a spider crawling out of your nest.
       </p>
       <br />
       <p>
@@ -132,7 +130,7 @@ useSeoMeta({
         are more focused on balance and precise footwork, since gravity pushes
         you towards the wall. If the route setters used the same kinds of holds
         they do on overhangs here, the routes would be way too easy (due to the
-        aforementioned gravity situation), so it's also famous for its miniscule
+        aforementioned gravity situation), so it's (in)famous for its miniscule
         holds that the naked eye cannot see. Here you can feel like a mountain
         goat hopping to the top, if you don't slip, which I do more than I would
         like to admit.
@@ -141,7 +139,7 @@ useSeoMeta({
       <p>
         The main takeaway here is that there is something every body type is
         naturally good at, and everyone has their strengths and weaknesses. This
-        to me makes it not only inclusive, but also an expression of self. If
+        to me not only makes it inclusive, but also an expression of self. If
         you'd like to argue that there exists no such thing as a self, then I'd
         probably agree with you, but the more philosophical stuff will have to
         wait for a future article.
@@ -150,12 +148,12 @@ useSeoMeta({
       <h3 class="mb-2">5. Community</h3>
       <p>
         I've been a part of some amazing communities over the years, having done
-        a diverse range of sports and hobbies, but in my personal experience
-        nothing compares to the warm hearted feeling I get every time I enter a
-        boulder gym. There are people of all ages, children to adolescents to
-        adults to elderly, trying the same problems and helping each other to
-        accomplish what they themselves didn't think they could do. This sense
-        of connection, everyone doing the same stuff in different ways with
+        a diverse range of sports and hobbies, but in my experience nothing
+        compares to the warm hearted feeling I get every time I enter a boulder
+        gym. There are people of all ages, children to adolescents to adults to
+        elderly, trying the same problems and helping each other to accomplish
+        what they themselves didn't think they could do. This sense of
+        connection, everyone doing the same stuff in different ways with
         different backgrounds, is such a beautiful thing to witness.
       </p>
       <br />
